034c6aeaa51319249ab2b3e504b5dd306aa00c46
Eryn Kelsey-Adkins <ekelseya@msudenver.edu>
2019-05-02 12:57:10 -0600

    Create Palette.kt

---
+++ app/src/main/java/com/ekelseya/palettedesign/Palette.kt
@@ -0,0 +1,6 @@
+package com.ekelseya.palettedesign
+
+import java.io.Serializable
+
+class Palette (val primaryBlock: ColorBlocks, val secondaryBlock: ColorBlocks, val tertiaryBlock: ColorBlocks, val accentBlock: ColorBlocks, val pName: String ) : Serializable {
+}
